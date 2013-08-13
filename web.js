6c59177f99ae4300312d95e3e27f5071fb6d9fe8
josecarmonagz <jose.carmonagz@gmail.com>
2013-08-13 03:45:53 +0000

    se agrego read file

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
+
+/*app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});*/
+
+var datos = fs.readFileSync('index.html','utf8');
+
+app.get('/', function(request, response) {
+  response.send(datos);
 });
 
 var port = process.env.PORT || 5000;
