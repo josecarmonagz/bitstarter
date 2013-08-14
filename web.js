c54aee5243e554d88130961d7edc7ada6951df4a
josecarmonagz <jose.carmonagz@gmail.com>
2013-08-14 19:42:20 +0000

    Prueba

--- web.js
+++ web.js
@@ -3,15 +3,15 @@ var app = express();
 app.use(express.logger());
 
 
-/*app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});*/
-
-var datos = fs.readFileSync('index.html');
-
 app.get('/', function(request, response) {
-  response.send(datos);
+  response.send('Hello World 2!');
 });
+
+//var datos = fs.readFileSync('index.html');
+
+/*app.get('/', function(request, response) {
+  response.send('Hola');
+});*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
